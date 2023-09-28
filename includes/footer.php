cc7b90b3ad17efd259500b94ece3ccc04983b9af
viktor-argirov <viktor.argirov@laplateforme.io>
2023-09-28 10:10:37 +0200

    Php

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer class="bg-light text-center text-lg-start">
+  <div class="container p-4">
+    <div class="row">
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <h5 class="text-uppercase"></h5>
+        <ul class="list-unstyled mb-0">
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
